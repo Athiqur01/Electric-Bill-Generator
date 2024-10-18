2626e2e1bd17c1557ef906b438ef422386c90d3f
Athiqur01 <athik.apece@gmail.com>
2024-10-18 08:06:37 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Component/Root/Root';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className='max-w-[1280px] mx-auto'>
+    <RouterProvider router={router} />
+    </div>
+  </StrictMode>,
+)
